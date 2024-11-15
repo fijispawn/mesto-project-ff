dd517430edf165a2680efb4819ac8d5fe403e54c
Mikhail Smirnov <m2004029@edu.misis.ru>
2024-11-15 15:24:49 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardReciever = (name, link) => {
+  let card = document.querySelector("#card-template");
+  let cloned = card.content.cloneNode(true);
+
+  let image = cloned.querySelector("img");
+  let title = cloned.querySelector(".card__title");
+
+  image.src = link;
+  image.alt = name;
+  title.textContent = name;
+
+  const deleteButton = cloned.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () =>{
+    let cardElement = deleteButton.closest('.card');
+    cardElement.remove();
+  })
+
+  return cloned;
+};
+
+function deleteCard(cardElement){
+cardElement.remove()
+}
+
+let cardContainer = document.querySelector('.places__list');
+
+initialCards.forEach((item) =>{
+    const cardElement = cardReciever(item.name, item.link);
+    cardContainer.append(cardElement);
+})
